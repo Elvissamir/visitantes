06caa06848eaa940267e5b9f32a8af2f0edd870f
antabra25 <antabra25@gmail.com>
2022-03-24 23:32:16 -0400

    Input agregados

--- src/components/InputLong.vue
+++ src/components/InputLong.vue
@@ -1,14 +1,32 @@
 <template>
+  <div class="control-form">
+    <label :for="fieldName" class="labelField">{{fieldName}}</label>
+    <input :id="fieldName" type="text" class="inputField">
+  </div>
 
 
 </template>
 
 <script>
 export default {
-  name: "InputLong"
+  name: "InputLong",
+  props:['fieldName'],
+  data(){
+    return{
+
+    }
+  }
 }
 </script>
 
 <style scoped>
+
+.inputField {
+  @apply w-[250px] h-[40px]
+}
+
+labelField {
+ @apply
+}
 
 </style>
